069bc0d75f259576ad6a7f083e0e2bb08b36340e
wuhao <wuhao>
2019-01-16 14:29:41 +0800

    some changes

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SalePlantform
 SalePlantform with jQuery
 说明：css文件共用./css/style.css
+      js文件单独引用，文件名与html对应  
 
 html文件及目录说明：
 ./index.html - 首页
--- js/index.js
+++ js/index.js
@@ -16,11 +16,11 @@ $("#search").click(function(){
 /*-------------滚动轮播------------*/
 var t = setInterval(function(){
     $("#roll1").animate({marginTop:-25},400,function(){
-        $("#roll1 p:last-child").after($("#roll1 p:first-child"));
+        $("#roll1 a:last-child").after($("#roll1 a:first-child"));
         $("#roll1").css({marginTop:0})
     })
     $("#roll2").animate({marginTop:-25},400,function(){
-        $("#roll2 p:last-child").after($("#roll2 p:first-child"));
+        $("#roll2 a:last-child").after($("#roll2 a:first-child"));
         $("#roll2").css({marginTop:0})
     })
 },2400)
